3d735b5ae7351ca1ee11e15c4b92f497a0b0bc8a
naelstrof <naelstrof@gmail.com>
2016-03-24 15:34:59 -0600

    asdf

--- Boggle/BoggleInteractor.cs
+++ Boggle/BoggleInteractor.cs
@@ -9,32 +9,46 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApplication1
 {
+    struct Player
+    {
+        string nickname;
+        int score;
+    }
+    struct GameState
+    {
+        public int gameID;
+        public string server;
+        public string nickname;
+        public string state;
+        public string board;
+        public int timeLimit;
+        public int timeLeft;
+        public string userToken;
+        public string status;
+        public List<Player> players;
+    }
     class BoggleAPI
     {
-        public string playerName { set; get; }
-        public string playerID { set; get; }
-        public string gameID { set; get; }
-        public double score { set; get; }
-        public string server { set; get; }
-
+        public GameState state;
         public BoggleAPI()
         {
-            server = "http://bogglecs3500s16.azurewebsites.net/";
-            playerName = "Loque";
+            state.server = "http://bogglecs3500s16.azurewebsites.net/";
+            state.nickname = "Loque";
+            state.status = "OK.";
         }
         public HttpClient CreateClient()
         {
             // Create a client whose base address is the GitHub server
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(this.server);
+            client.BaseAddress = new Uri(this.state.server);
             return client;
         }
-        async Task<int> CreateUserASync()
+        async Task<bool> CreateUserASync( GameState foo, string nickname )
         {
             using (HttpClient client = CreateClient())
             {
                 dynamic data = new ExpandoObject();
-                data.Nickname = playerName;
+                data.Nickname = nickname;
                 StringContent content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
                 HttpResponseMessage response = await client.PostAsync("/BoggleService.svc/users", content);
 
@@ -42,12 +56,40 @@ namespace WindowsFormsApplication1
                 {
                     string result = await response.Content.ReadAsStringAsync();
                     dynamic player = JsonConvert.DeserializeObject(result);
-                    playerID = player.UserToken;
-                    return (int)response.StatusCode;
+                    state.userToken = player.UserToken;
+                    state.status = "Created new user " + state.nickname + " successfully!";
+                    return true;
                 }
                 else
                 {
-                    throw new Exception("Error creating user: " + response.StatusCode + ", " + response.ReasonPhrase);
+                    state.status = "Error creating user: " + response.StatusCode + ", " + response.ReasonPhrase;
+                    return false;
+                }
+            }
+        }
+        async Task<bool> JoinGameASync( int timeLimit )
+        {
+            using (HttpClient client = CreateClient())
+            {
+                dynamic data = new ExpandoObject();
+                data.UserToken = state.userToken;
+                data.TimeLimit = timeLimit;
+
+                StringContent content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("/BoggleService.svc/games", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    dynamic game = JsonConvert.DeserializeObject(result);
+                    state.gameID = game.GameID;
+                    state.status = "Joined game " + state.gameID + " successfully.";
+                    return true;
+                }
+                else
+                {
+                    state.status = "Error Joining game: " + response.StatusCode + ", " + response.ReasonPhrase;
+                    return false;
                 }
             }
         }
