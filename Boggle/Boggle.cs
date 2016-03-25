d68b2285f9050935154350d517bbb94acc247fc8
aptrz <apetrizze@gmail.com>
2016-03-24 23:51:52 -0600

    Pushing

--- Boggle/Boggle.cs
+++ Boggle/Boggle.cs
@@ -77,15 +77,58 @@ namespace WindowsFormsApplication1
         }
         public Boggle()
         {
+            set
+            {
+                if (value.Length == 16)
+                {
+                    label1.Text = value[0].ToString();
+                    label2.Text = value[1].ToString();
+                    label3.Text = value[2].ToString();
+                    label4.Text = value[3].ToString();
+                    label5.Text = value[4].ToString();
+                    label6.Text = value[5].ToString();
+                    label7.Text = value[6].ToString();
+                    label8.Text = value[7].ToString();
+                    label9.Text = value[8].ToString();
+                    label10.Text = value[9].ToString();
+                    label11.Text = value[10].ToString();
+                    label12.Text = value[11].ToString();
+                    label13.Text = value[12].ToString();
+                    label14.Text = value[13].ToString();
+                    label15.Text = value[14].ToString();
+                    label16.Text = value[15].ToString();
+        }
+                else
+                {
+                    throw new Exception("Invalid Value Length");
+                }
+            }
+        }
+        public string Message
+        {
+            set
+            {
+                toolStripStatusLabel1.Text = value;
+            }
+        }
+        public string Log
+        {
+            set
+            {
+                textBox2.Text = value;
+            }
+        }
+        public string Score
+        {
+            set
+        {
+                scoreLabel.Text = "Score" + value;
+            }
+        }
+
+        public Boggle()
+        {
             InitializeComponent();
-        }
-
-        private void Boggle_Load(object sender, EventArgs e)
-        {
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -96,9 +139,6 @@ namespace WindowsFormsApplication1
             {
                 WordEvent( w );
             }
-        }
-        private void label1_Click(object sender, EventArgs e)
-        {
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -123,7 +163,47 @@ namespace WindowsFormsApplication1
             }
         }
 
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (JoinGameEvent != null)
+            {
+                JoinGameEvent();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (CancelGameEvent != null)
+            {
+                CancelGameEvent();
+            }
+        }
+
+
+
+
+        //The following functions I think I accidentally created but I am not ssure if they are needed for anything
+        //or not so I just left them here
+
+
+        private void Boggle_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+            }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label17_Click(object sender, EventArgs e)
         {
 
         }
